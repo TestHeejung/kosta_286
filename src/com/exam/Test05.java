a8bd6ea969ab6177d2906cb708522a1cffe61d89
heejung <8253jang@daum.net>
2024-08-22 22:05:41 +0900

    clone에서 test05수정, tst06수정

--- src/com/exam/Test05.java
+++ src/com/exam/Test05.java
@@ -1,5 +1,7 @@
 package com.exam;
 
 public class Test05 {
-
+  public void cloneTest() {
+	  System.out.println("clone에서 수정함");
+  }
 }
--- src/com/exam/Test06.java
+++ src/com/exam/Test06.java
@@ -1,5 +1,7 @@
 package com.exam;
 
 public class Test06 {
-
+   public void clone() {
+	   System.out.println("ddd");
+   }
 }
